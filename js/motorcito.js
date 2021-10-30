be51fed29afac0f0b001faaf70f188b4bfdfbd99
ElvisTV <elvistezen@outlook.es>
2021-10-30 13:39:11 -0500

    first commit and project upload

---
+++ js/motorcito.js
@@ -0,0 +1,31 @@
+var estadoLicuadora = "apagada";
+var sonidoLicuadora = document.getElementById("blender-sound");
+var botonLicuadora = document.getElementById("blender-button-sound");
+var licuadora = document.getElementById("blender");
+
+function controlarLiuadora() {
+    if (estadoLicuadora == "apagada") {
+        estadoLicuadora = "encendido";
+        licuadora.classList.add("active");
+        ejecutaSonido();
+       /*  console.log("me prendiste") */
+    } else {
+        estadoLicuadora = "apagada";
+        licuadora.classList.remove("active");
+        ejecutaSonido();
+        /* console.log("me apagaste"); */
+    }
+}
+
+function ejecutaSonido() {
+    if (sonidoLicuadora.paused) {
+        botonLicuadora.play();
+        sonidoLicuadora.play();
+        sonidoLicuadora.onreset();
+    } else {
+        botonLicuadora.play();
+        sonidoLicuadora.pause();
+        sonidoLicuadora.currentTime = 0; //para resetear el audio
+        sonidoLicuadora.onreset();
+    }
+}
